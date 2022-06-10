b1bb66e21b7f5a3af1ec4e9da3c2a930d26354dd
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-06-10 16:45:19 +0700

    master

--- bin/week4_class102.dart
+++ bin/week4_class102.dart
@@ -1,9 +1,10 @@
 import 'week4_class101.dart';
 
-void main(){
+void main() {
   Student std1 = Student("Mark",30,101); //Object Creation
-  Student std2 = Student("Zee",19,102);
-  Student std3 = Student("Jeff",25,103);
+  Student std2 = Student("Elon",25,102);
+  Student std3 = Student("Jeff",24,103);
+  Student std4 = Student.message("Putin",50,104,"IT");
 }
 
 class Student {
@@ -11,17 +12,27 @@ class Student {
   String? stdName;
   int? stdAge;
   int? stdId;
+  String? branch;
 
-  //Constructor of class
+  //Constructor/Method of class
   Student(String name,int age,int id){
     stdName = name;
     stdAge = age;
-    stdId = id;
+    stdId  = id;
+
     print('Name:$stdName,Age:$stdAge,ID:$stdId');
   }
+  //Named Constructor
+  Student.message(String name,int age,int id,String br){
+    stdName = name;
+    stdAge = age;
+    stdId  = id;
+    branch = br;
+    print('Name:$stdName,Age:$stdAge,ID:$stdId,Branch:$branch');
+  }
   //Method of Class
-  void study(){
-    print("studying");
+  void study() {
+    print("Studying");
   }
 
   void play(){
